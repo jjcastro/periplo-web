7369cd6a7e71a61f6c2df6d8e9167b9804e04b19
J.J. Castro <jj.castro10@uniandes.edu.co>
2017-08-21 01:13:44 -0500

    Web for the app

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	'port': process.env.PORT || 8080,
-	'secret': 'ilovescotchscotchyscotchscotch',
-  'postgresUrl': 'postgres://gbsmeuemrupsay:2bc23df07295ee2573b6549db9e0b87a266f662ab584958c03bda71692cbfffc@ec2-23-21-220-152.compute-1.amazonaws.com:5432/d55nds4m5dtgt1'
-};
--- public/app/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('porcApp', ['ngAnimate', 'ui.bootstrap', 'app.routes', 'authService', 'mainCtrl', 'overviewCtrl', 'appCtrl', 'transactionService'])
-
-// application configuration to integrate token into requests
-.config(function($httpProvider) {
-
-	// attach our auth interceptor to the http requests
-	$httpProvider.interceptors.push('AuthInterceptor');
-
-});
